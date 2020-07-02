5bb62f53500aa0af16791388218bb49e28e6ab42
tdevilleduc <thomas.deville-duc.externe@amdm.fr>
2020-07-02 18:07:50 +0200

    initial commit

---
+++ src/test/java/com/tdevilleduc/samples/performance/PerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdevilleduc.samples.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
